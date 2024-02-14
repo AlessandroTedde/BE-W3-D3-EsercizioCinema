ebf157b623fa332797508318960949425901fc5b
AlessandroTedde <148778362+AlessandroTedde@users.noreply.github.com>
2024-02-14 16:21:43 +0100

    Aggiungere i file di progetto.

---
+++ EsercizioCinema/Home.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cinema
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string Cinema = ConfigurationManager.ConnectionStrings["CinemaDB"].ConnectionString;
+            SqlConnection conn = new SqlConnection(Cinema);
+            string selectedValue = DropDownList2.SelectedValue;
+
+            try
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand();
+                command.Connection = conn;
+                command.CommandText = "INSERT INTO Sale(Nome, Cognome, SalaNord, SalaSud, SalaEst, Ridotto) VALUES (@Nome, @Cognome, @SalaNord, @SalaSud, @SalaEst, @Ridotto)";
+                command.Parameters.AddWithValue("@Nome", Nome.Text);
+                command.Parameters.AddWithValue("@Cognome", Cognome.Text);
+                command.Parameters.AddWithValue("@SalaNord", selectedValue == "Nord" ? true : false);
+                command.Parameters.AddWithValue("@SalaSud", selectedValue == "Sud" ? true : false);
+                command.Parameters.AddWithValue("@SalaEst", selectedValue == "Est" ? true : false);
+                command.Parameters.AddWithValue("@Ridotto", CheckBox1.Checked);
+
+                command.ExecuteNonQuery();
+                Response.Write("Inserito con successo");
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Errore");
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
